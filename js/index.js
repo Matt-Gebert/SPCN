e550fbd8e19cd95e2925cf7e88a0a6adf688d914
Matt-Gebert <Mattheuu.Gebert@gmail.com>
2016-09-15 01:08:29 +1000

    Navigation and Contact Bar
    Added at the bottom a contact/navigation bar to the index template.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 //Scrolling Functions:
 $(".parallax").scroll(function() {
-    //Adds Functionality to the Header Bar.
+    //Scrolling Fading for scroll-img
+        $(".scroll-img#scroll-img1").css("opacity", 0.7 - 0.4*$(this).scrollTop() / 600);
+
         if($(this).scrollTop() > 1){
             $('div#ban-container').addClass("sticky");
             $('img#ban-img').addClass("sticky");
         } else {
-            $('div#ban-container').removeClass("sticky");
+            $('div#ban-container').removeClass("sticky");//.style("max-height",48px);
             $('img#ban-img').removeClass("sticky");
         }
-
-    //Scrolling Fading for scroll-img
-        $(".scroll-img#scroll-img1").css("opacity", 0.7 - 0.4*$(this).scrollTop() / 600);
-
  });
+
+
+ $(window).scroll(function() {
+    //Adds Functionality to the Header Bar.
+
+
+ })
